7dd27609eb9eac3429a606ad14ec561446a50eac
PriyaPal001 <vu3t1s2021032@pvppcoe.ac.in>
2024-01-13 15:55:03 +0530

    base layout

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import iconn from '$lib/images/p1.png';
+	let navs = [
+		{
+			name: 'Dashboard',
+			link: '#'
+		},
+		{
+			name: 'Projects',
+			link: '#'
+		},
+		{
+			name: 'About Us',
+			link: '#'
+		},
+		{
+			name: 'Settings',
+			link: '#'
+		}
+	];
+</script>
+
+<div class="bg-black h-screen">
+
+</div>
